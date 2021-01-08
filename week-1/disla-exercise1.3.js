a07750f8f6b8bb29fd1f1d4cbb5426f16bceb7b8
Jonathan Disla <jond@Jonathans-MacBook-Air.local>
2021-01-08 00:03:27 -0500

    update exercise 1.3

--- week-1/disla-exercise1.3.js
+++ week-1/disla-exercise1.3.js
@@ -1,9 +1,9 @@
 /*
 ============================================
-; Title:  Exercise 1.3 -
+; Title:  Exercise 1.3
 ; Author: Jonathan Disla
 ; Date:    January 4 2021
-; Description: 
+; Description: Classes Refresher
 ;===========================================
 */
 
@@ -20,23 +20,29 @@ BEGIN ASSIGNMENT
 ===============================================
 */
 
+//this function object cellPhone takes in parameters and
+//declares three methods.
 function cellPhone(manufacturer, model, color, price){
     this.manufacturer = manufacturer;
     this.model = model;
     this.color = color;
     this.price = price;
 
+    //returns the property price from above
     this.getPrice = function(){
         return this.price;
     }
+    //returns the model property from above
     this.getModel = function(){
         return this.model;
     }
+    //returns all the information in form of a string
     this.getDetails = function(){
         return "Manufacturer: " + this.manufacturer + "\nModel: " + this.getModel() + "\nColor: " + this.color + "\nPrice: " + this.getPrice();
     }
 }
 
+//assign values to the parameters in the object function above.
 var samsung = new cellPhone("Samsung", "Galaxy S20", "black", 349.99);
 
 console.log(samsung.getDetails());
