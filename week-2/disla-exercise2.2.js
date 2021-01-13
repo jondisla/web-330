93e89e705b8b4e2bf09a811a7fee3d41b16f54d2
Jonathan Disla <jon@Jonathans-Mac-mini.local>
2021-01-12 20:21:01 -0500

    up 2.2

--- week-2/disla-exercise2.2.js
+++ week-2/disla-exercise2.2.js
@@ -3,7 +3,9 @@
 ; Title:  Exercise 2.2
 ; Author: Jonathan Disla
 ; Date:    January 13 2021
-; Description: Classes Refresher
+; Description: Using object literals to create
+; a function and create a new instance of the
+; object.
 ;===========================================
 */
 
@@ -15,15 +17,45 @@ const header = require("../disla-header.js");
 console.log(header.display("Jonathan", "Disla", "Exercise 2.2"));
 
 /*
-===============================================
-BEGIN ASSIGNMENT
-===============================================
+ Expected output:
+
+ FirstName LastName
+ <AssignmentName>
+ <Today's Date>
+
+ The person's full name is '<fullname>.'
+ The person's age is '<age>.'
+
 */
 
+// start program
+
+//declare an object literal person
 var person = {
-    getAge = function(){
+    //a function that returns the person's age
+    getAge : function(){
         this.age;
     }
 }
 
-var jon = 
+//second object literal using object.create
+//to create a new person
+var jon = Object.create(person,{
+    //assign the properties age and full name
+    //to the new person
+    "age":{
+        "value": "26"
+    },
+    "fullname":{
+        "value": "Jon Jones"
+    }
+})
+
+//call the getAge function for the name Jon
+jon.getAge();
+//Log the results
+console.log("Full name: " + jon.fullname);
+console.log("age: " + jon.age);
+
+// end program
+
