7840c867a49ba7fc2c72d974054bf0a6616bb6da
Jonathan Disla <jond@Jonathans-MacBook-Air.local>
2021-01-12 19:00:19 -0500

    week 2

---
+++ week-2/disla-exercise2.2.js
@@ -0,0 +1,29 @@
+/*
+============================================
+; Title:  Exercise 2.2
+; Author: Jonathan Disla
+; Date:    January 13 2021
+; Description: Classes Refresher
+;===========================================
+*/
+
+/**
+* header: constant that holds the import from disla-header.js file
+* console.log: logs the function from the disla-header.js file
+*/
+const header = require("../disla-header.js");
+console.log(header.display("Jonathan", "Disla", "Exercise 2.2"));
+
+/*
+===============================================
+BEGIN ASSIGNMENT
+===============================================
+*/
+
+var person = {
+    getAge = function(){
+        this.age;
+    }
+}
+
+var jon = 
